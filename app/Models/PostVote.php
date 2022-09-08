a4526fbcd3581fc10af22f63096ce48ce4ed2ea4
akobey <akobey@gmail.com>
2022-09-08 16:05:55 +0300

    tuto 4

--- app/Models/PostVote.php
+++ app/Models/PostVote.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class PostVote extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'post_id',
+        'vote',
+    ];
+
+
 }
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -8,7 +8,6 @@ import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
 
 const form = useForm({
     name: '',
-    username: '',
     email: '',
     password: '',
     password_confirmation: '',
@@ -31,12 +30,6 @@ const submit = () => {
                 <InputLabel for="name" value="Name" />
                 <TextInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus autocomplete="name" />
                 <InputError class="mt-2" :message="form.errors.name" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="username" value="Username" />
-                <TextInput id="username" type="text" class="mt-1 block w-full" v-model="form.username" required autofocus autocomplete="username" />
-                <InputError class="mt-2" :message="form.errors.username" />
             </div>
 
             <div class="mt-4">
